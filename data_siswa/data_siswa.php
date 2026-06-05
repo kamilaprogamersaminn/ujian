2266777b94e1f4b6c2d6d1bfb60dd4c7eefe2730
Kamila <kamilaprogamersaminn@example.com>
2026-06-05 17:25:01 +0700

    commit kesembilan kalinya

--- data_siswa/data_siswa.php
+++ data_siswa/data_siswa.php
@@ -40,7 +40,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Siswa - PPDB</title>
+    <title>Data Siswa</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../css/data.css">
 </head>
--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PPDB - SMK Riyadhul Ulum Ujungbatu</title>
+    <title>SMK Riyadhul Ulum Ujungbatu</title>
     <link rel="stylesheet" href="css/index1.css">
 </head>
 <body>
